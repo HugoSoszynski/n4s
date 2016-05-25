2161e9a166622eeecc4923676dc65e4b328d1d11
loens_g <gregoire.loens@epitech.eu>
2016-05-25 18:05:34 +0200

    fonction de parsing

---
+++ sources/responsive/parse_answer.c
@@ -0,0 +1,50 @@
+/*
+** parse_answer.c for CPE_2015_n4s in /home/loens_g/Rendu/CPE_2015_n4s/sources/parse_answer.c
+**
+** Made by Grégoire LOENS
+** Login   <gregoire.loens@epitech.eu>
+**
+** Started on  Wed May 25 15:11:00 2016 Grégoire LOENS
+** Last update Wed May 25 15:11:00 2016 Grégoire LOENS
+*/
+
+#include	"ia.h";
+#include	"struct.h"
+
+int		parse_answer_for_type_2(t_car *car, char *answer)
+{
+  char		**answer_tab;
+  int		tab_counter;
+  int		counter;
+
+  counter = 0;
+  tab_counter = 3;
+  answer_tab = my_str_to_wordtab(answer);
+  if (answer_tab[1][0] == 'K')
+  {
+    write(2, &answer_tab[2], my_strlen(answer_tab[2]));
+    return (ERROR);
+  }
+  while (counter < 32)
+  {
+    car->lidar[counter] = my_getfloat(answer_tab[tab_counter]);
+    counter++;
+    tab_counter++;
+  }
+  free(answer_tab);
+  return (SUCCESS);
+}
+
+int		parse_answer_for_type_1(char *answer)
+{
+  char		**answer_tab;
+
+  answer_tab = my_str_to_wordtab(answer);
+  if (answer_tab[1][0] == 'K')
+  {
+    write(2, &answer_tab[2], my_strlen(answer_tab[2]));
+    return (ERROR);
+  }
+  free(answer_tab);
+  return (SUCCESS);
+}
