177a0a0c8789935d0aef1f1e1d226ed71ec96121
Hugo SOSZYNSKI <hugo.soszynski@epitech.eu>
2016-05-29 14:41:55 +0200

    Legere correction du parsing des reposes

--- sources/responsive/parse_answer.c
+++ sources/responsive/parse_answer.c
@@ -50,6 +50,8 @@ int		parse_answer_for_type_1(t_car *car, char *answer)
     write(2, &answer_tab[2], my_strlen(answer_tab[2]));
     return (ERROR);
   }
+  if (answer_tab[3][0] == 'T')
+    car->track_cleared = true;
   free(answer_tab);
   return (SUCCESS);
 }
